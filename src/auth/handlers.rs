7014581607546af34521130a1c951ad2be9e2447
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:20:44 +0700

    Add login_handler in handlers.rs🫸🌀✏️📗🐧🐳

--- src/auth/handlers.rs
+++ src/auth/handlers.rs
@@ -34,3 +34,37 @@ pub async fn register_handler(mut _req: UserCreateRequest, _env: Environment) ->
         }
     }
 }
+
+pub async fn login_handler(_req: LoginRequest, _env: Environment) -> WebResult<impl Reply> {
+    let user_option = match users::service::get_user_by_email(&_req.email, _env.db()).await {
+        Ok(None) => {
+            println!("[login_handler] Error authenticating user {:?}", &_req.email);
+            return Err(warp::reject::custom(AuthError::InvalidCredentials))
+            // return Ok(warp::reply::json(&json!({"status":"error", "message":"Email or password unknown"})));
+        },
+        Ok(existing) => existing,
+        _ => {
+            println!("[login_handler] Error authenticating user {:?}", &_req.email);
+            return Ok(warp::reply::json(&json!({"status":"error", "message":"Email or password unknown"})));
+        },
+    };
+
+    let user = user_option.unwrap();
+    let is_valid = _env
+        .argon()
+        .verifier()
+        .with_hash(&user.password)
+        .with_password(&_req.password)
+        .verify()
+        .or(Err(warp::reject::custom(AuthError::ArgonError)))?;
+
+    if !is_valid {
+        println!("[login_handler] Invalid credentials for user {:?}", &_req.email);
+        return Err(warp::reject::custom(AuthError::InvalidCredentials))
+    }
+
+    println!("[login_handler] Authenticated user '{}' ({})", &user.email, &user.role);
+    let token = create_jwt(&user.id.to_string(), &Role::from_str(&user.role)).unwrap();
+    let body = LoginResponse::from_user(user, token);
+    return Ok(warp::reply::json(&body));
+}
