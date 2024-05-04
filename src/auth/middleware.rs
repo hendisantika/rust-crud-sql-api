deb42f4d30ff54b8ec85bced4db16c6333be8864
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:21:26 +0700

    Add authenticated in middleware.rs🫸🌀✏️📗🐧🐳

--- src/auth/middleware.rs
+++ src/auth/middleware.rs
@@ -6,3 +6,9 @@ use crate::auth::{BEARER, JWT_SECRET};
 use crate::auth::models::{AuthUser, Claims, Role};
 use crate::error::AppError;
 
+// Authentication middleware
+pub fn authenticated() -> impl Filter<Extract=(AuthUser, ), Error=warp::reject::Rejection> + Clone {
+    warp::header::headers_cloned()
+        .map(move |headers: HeaderMap<HeaderValue>| headers)
+        .and_then(authorize_any)
+}
