99084af96780310dcfae6018031d3f1c21e53954
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:31:05 +0700

    Add Args🫸🌀✏️📗🐧🐳

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -11,3 +11,29 @@ pub struct Environment {
     config: Args,
     argon: Argon,
 }
+
+#[derive(Clone, Clap, Debug)]
+#[clap(
+name = "demo-api",
+rename_all = "kebab-case",
+rename_all_env = "screaming-snake"
+)]
+pub struct Args {
+    #[clap(short, long)]
+    debug: bool,
+
+    #[clap(required = true, short = 'D', long, env)]
+    database_url: String,
+
+    #[clap(required = true, long, env)]
+    jwt_secret: String,
+    #[clap(required = true, long, env)]
+    argon_secret: String,
+    #[clap(long, env)]
+    argon_iterations: Option<u32>,
+    #[clap(long, env)]
+    argon_memory_size: Option<u32>,
+
+    #[clap(default_value = "0.0.0.0:8080", env)]
+    pub host: SocketAddr,
+}
