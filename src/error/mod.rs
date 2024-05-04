fb3f601285e9b0736e760ec7255978ea561a3084
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:16:27 +0700

    Add AuthError enum🫸🌀✏️📗🐧🐳

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -2,3 +2,13 @@ use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
 pub mod handlers;
+
+#[derive(Error, Debug)]
+pub enum AuthError {
+    #[error("invalid credentials")]
+    InvalidCredentials,
+    #[error("could not hash password")]
+    ArgonError,
+}
+
+impl warp::reject::Reject for AuthError {}
