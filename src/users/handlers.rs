2f2e71e15750ec028c1bda3e0dfcf978a661004e
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:32:44 +0700

    Add get_users_handler in users handlers.rs🫸🌀✏️📗🐧🐳

--- src/users/handlers.rs
+++ src/users/handlers.rs
@@ -8,3 +8,9 @@ use crate::users::models::{PasswordUpdateRequest, UserCreateRequest, UserUpdateR
 use crate::users::service;
 use crate::WebResult;
 
+// Returns all users
+pub async fn get_users_handler(_env: Environment, _user: AuthUser) -> WebResult<impl Reply> {
+    println!("[get_users_handler] Action performed by {}", _user);
+    let result = service::get_users(_env.db()).await?;
+    Ok(warp::reply::json(&result))
+}
