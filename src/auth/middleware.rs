d43da0547e3f34598681c5c4fa3eaf12cec6e733
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:22:03 +0700

    Add authorize in middleware.rs🫸🌀✏️📗🐧🐳

--- src/auth/middleware.rs
+++ src/auth/middleware.rs
@@ -37,3 +37,23 @@ pub fn with_auth(role: Role) -> impl Filter<Extract=(AuthUser, ), Error=warp::re
         .map(move |headers: HeaderMap<HeaderValue>| (role.clone(), headers))
         .and_then(authorize)
 }
+
+async fn authorize((role, headers): (Role, HeaderMap<HeaderValue>)) -> WebResult<AuthUser> {
+    match jwt_from_header(&headers) {
+        Ok(jwt) => {
+            let decoded = jsonwebtoken::decode::<Claims>(
+                &jwt,
+                &jsonwebtoken::DecodingKey::from_secret(JWT_SECRET),
+                &jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::HS512),
+            )
+                .map_err(|_| warp::reject::custom(AppError::JWTTokenError))?;
+
+            if role == Role::Admin && Role::from_str(&decoded.claims.role) != Role::Admin {
+                return Err(warp::reject::custom(AppError::NoPermissionError));
+            }
+            let user = AuthUser::new(decoded.claims.sub, decoded.claims.role);
+            Ok(user)
+        }
+        Err(e) => return Err(warp::reject::custom(AppError::from(e))),
+    }
+}
