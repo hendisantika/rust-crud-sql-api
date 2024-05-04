1b7fb7ecc869f1ab796880b9ed80ebdd45bc2756
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:23:51 +0700

    Add AuthUser struct in models.rs🫸🌀✏️📗🐧🐳

--- src/auth/models.rs
+++ src/auth/models.rs
@@ -3,3 +3,10 @@ use serde::{Deserialize, Serialize};
 
 use crate::users::models::User;
 
+// Parsed user from JWT session that is injected in authenticated handlers
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct AuthUser {
+    pub id: String,
+    pub role: Role,
+    pub login_at: DateTime<Utc>,
+}
