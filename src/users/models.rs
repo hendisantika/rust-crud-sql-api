9fb1305aaf948f4a90953583630e29ead03d0f0e
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:34:58 +0700

    Add User struct🫸🌀✏️📗🐧🐳

--- src/users/models.rs
+++ src/users/models.rs
@@ -3,3 +3,14 @@ use serde::{Deserialize, Serialize};
 
 use crate::auth::models::Role;
 
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct User {
+    pub id: uuid::Uuid,
+    pub email: String,
+    pub name: String,
+    #[serde(skip_serializing)]
+    pub password: String,
+    pub role: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: Option<DateTime<Utc>>,
+}
