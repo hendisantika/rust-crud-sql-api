53cdb2bea620c23dfebdeabf7af3c37e22440599
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:07:50 +0700

    Add update_article_handler🫸🌀✏️📗🐧🐳

--- src/articles/handlers.rs
+++ src/articles/handlers.rs
@@ -31,3 +31,16 @@ pub async fn create_article_handler(mut _req: Article, _env: Environment, _user:
     println!("[create_article_handler] Created article '{}'", &_req.title.unwrap());
     Ok(warp::reply::json(&json!({"status":"success", "message":"Article saved"})))
 }
+
+pub async fn update_article_handler(_req: Article, _env: Environment, _user: AuthUser) -> WebResult<impl Reply> {
+    let mut original = service::get_article_by_id(_req.id.unwrap(), _env.db()).await?.unwrap();
+    original.title = Some(_req.title.unwrap());
+    original.content = Some(_req.content.unwrap());
+    original.url = Some(_req.url.unwrap());
+    original.in_home = Some(_req.in_home.unwrap());
+    original.updated_at = Some(chrono::Utc::now());
+
+    let _result = service::update_article(&original, _env.db()).await?;
+    println!("[update_article_handler] id={}, title={}", &original.id.unwrap(), &original.title.unwrap());
+    Ok(warp::reply::json(&json!({"status":"success", "message":"Article updated"})))
+}
