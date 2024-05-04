d10fd7348f1a7dd5762759ad6e02d310060be6c6
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:32:56 +0700

    Add get_user_by_id_handler in users handlers.rs🫸🌀✏️📗🐧🐳

--- src/users/handlers.rs
+++ src/users/handlers.rs
@@ -14,3 +14,11 @@ pub async fn get_users_handler(_env: Environment, _user: AuthUser) -> WebResult<
     let result = service::get_users(_env.db()).await?;
     Ok(warp::reply::json(&result))
 }
+
+// Returns user with given id
+pub async fn get_user_by_id_handler(_id: String, _env: Environment, _user: AuthUser) -> WebResult<impl Reply> {
+    let uuid = uuid::Uuid::parse_str(&_id).unwrap();
+    let _result = service::get_user_by_id(uuid, _env.db()).await?;
+    println!("[get_user_by_id_handler] id={}, email={}", _id, &_result.clone().unwrap().email);
+    Ok(warp::reply::json(&_result))
+}
