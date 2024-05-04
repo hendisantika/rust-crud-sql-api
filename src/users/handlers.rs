9a1197bf8c2035bc38be98584b6ed1e816cea7ca
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:33:11 +0700

    Add user_create_handler in users handlers.rs🫸🌀✏️📗🐧🐳

--- src/users/handlers.rs
+++ src/users/handlers.rs
@@ -22,3 +22,31 @@ pub async fn get_user_by_id_handler(_id: String, _env: Environment, _user: AuthU
     println!("[get_user_by_id_handler] id={}, email={}", _id, &_result.clone().unwrap().email);
     Ok(warp::reply::json(&_result))
 }
+
+// Creates new user. Same logic as in registration service.
+pub async fn user_create_handler(mut _req: UserCreateRequest, _env: Environment, _user: AuthUser) -> WebResult<impl Reply> {
+    match service::get_user_by_email(&_req.email, _env.db()).await {
+        Ok(None) => (),
+        Ok(existing) => {
+            println!("[user_create_handler] User {} already exists", &existing.unwrap().email);
+            return Ok(warp::reply::json(&json!({"status":"error", "message":"Unable to create user, email already registered"})));
+        }
+        _ => (),
+    }
+
+    let hash = _env.argon().hasher().with_password(&_req.password).hash().unwrap();
+    _req.password = hash;
+    _req.role = Some(Role::User);
+
+    let email = _req.email.clone();
+    match service::create_user(_req, _env.db()).await {
+        Err(e) => {
+            println!("[user_create_handler] Error creating user {}: {:?}", &email, e.message);
+            return Err(warp::reject::custom(UserError::CreateError));
+        }
+        _ => {
+            println!("[user_create_handler] User creation successful: {:?}", &email);
+            return Ok(warp::reply::json(&json!({"status": "success"})));
+        }
+    }
+}
