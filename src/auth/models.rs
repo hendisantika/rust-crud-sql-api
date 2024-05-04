44108eea6c0c107cd80f040ac672e2362c02a91a
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:24:05 +0700

    Add AuthUser in models.rs🫸🌀✏️📗🐧🐳

--- src/auth/models.rs
+++ src/auth/models.rs
@@ -10,3 +10,13 @@ pub struct AuthUser {
     pub role: Role,
     pub login_at: DateTime<Utc>,
 }
+
+impl AuthUser {
+    pub fn new(id: String, role: String) -> AuthUser {
+        AuthUser {
+            id: id,
+            role: Role::from_str(&role),
+            login_at: Utc::now(),
+        }
+    }
+}
