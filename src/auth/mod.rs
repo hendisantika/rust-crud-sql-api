17b7d44bf43597ca516fd6b3954a3967737e54f6
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:23:10 +0700

    Add create_jwt in mod.rs🫸🌀✏️📗🐧🐳

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -8,3 +8,19 @@ pub mod routes;
 
 const BEARER: &str = "Bearer ";
 const JWT_SECRET: &[u8] = b"secret";
+
+pub fn create_jwt(uid: &str, role: &Role) -> Result<String> {
+    let expiration = chrono::Utc::now()
+        .checked_add_signed(chrono::Duration::seconds(259200))
+        .expect("valid timestamp")
+        .timestamp();
+
+    let claims = Claims {
+        sub: uid.to_owned(),
+        role: role.to_string(),
+        exp: expiration as usize,
+    };
+    let header = jsonwebtoken::Header::new(jsonwebtoken::Algorithm::HS512);
+    jsonwebtoken::encode(&header, &claims, &jsonwebtoken::EncodingKey::from_secret(JWT_SECRET))
+        .map_err(|_| AppError::JWTTokenCreationError)
+}
