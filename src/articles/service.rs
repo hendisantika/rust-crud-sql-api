a6f4ce81bf9d81eb3f326eeeef9b4dae57bd4c1e
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:12:58 +0700

    Add get_article_by_id in service.rs🫸🌀✏️📗🐧🐳

--- src/articles/service.rs
+++ src/articles/service.rs
@@ -5,3 +5,16 @@ use warp::Rejection;
 
 use crate::articles::models::{Article, Comment};
 
+pub async fn get_article_by_id(_id: uuid::Uuid, connection: &PgPool) -> Result<Option<Article>, Rejection> {
+    println!("[get_article_by_id] _id: {}", _id);
+    let result = query_as_unchecked!(
+        Article,
+        r#"SELECT id, title, url, content, created_at, updated_at, in_home FROM articles WHERE id=$1"#,
+        &_id
+    )
+        .fetch_one(connection)
+        .await
+        .map_err(|_e| { anyhow::Error::new(_e) })
+        .ok();
+    Ok(result)
+}
