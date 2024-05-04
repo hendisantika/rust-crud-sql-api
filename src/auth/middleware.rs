51dd8154dc82ca3cbe1217cedc8b62cbf107d3ab
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:21:39 +0700

    Add authorize_any in middleware.rs🫸🌀✏️📗🐧🐳

--- src/auth/middleware.rs
+++ src/auth/middleware.rs
@@ -12,3 +12,21 @@ pub fn authenticated() -> impl Filter<Extract=(AuthUser, ), Error=warp::reject::
         .map(move |headers: HeaderMap<HeaderValue>| headers)
         .and_then(authorize_any)
 }
+
+// Decodes JWT from header, checks its validity and assembles User object to be passed to the handlers
+async fn authorize_any(headers: HeaderMap<HeaderValue>) -> WebResult<AuthUser> {
+    match jwt_from_header(&headers) {
+        Ok(jwt) => {
+            let decoded = jsonwebtoken::decode::<Claims>(
+                &jwt,
+                &jsonwebtoken::DecodingKey::from_secret(JWT_SECRET),
+                &jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::HS512),
+            )
+                .map_err(|_| warp::reject::custom(AppError::JWTTokenError))?;
+
+            let user = AuthUser::new(decoded.claims.sub, decoded.claims.role);
+            Ok(user)
+        }
+        Err(e) => return Err(warp::reject::custom(AppError::from(e))),
+    }
+}
