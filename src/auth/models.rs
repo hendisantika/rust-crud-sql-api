cdcda406393ebe61f6fd90b0e96f0f4651793cd6
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 07:26:05 +0700

    Add Display in models.rs🫸🌀✏️📗🐧🐳

--- src/auth/models.rs
+++ src/auth/models.rs
@@ -77,3 +77,12 @@ impl Role {
         }
     }
 }
+
+impl std::fmt::Display for Role {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Role::User => write!(f, "User"),
+            Role::Admin => write!(f, "Admin"),
+        }
+    }
+}
